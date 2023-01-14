0fb033199f65739bcb0623d02336c671712fc90a
Nuran Albayrak <nuranalbayrak03@gmail.com>
2023-01-14 23:51:15 +0300

    commit

--- src/test/java/day14/C03_ExtentReports.java
+++ src/test/java/day14/C03_ExtentReports.java
@@ -4,6 +4,7 @@ import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import utilities.TestBase;
 
 import java.text.SimpleDateFormat;
@@ -16,7 +17,18 @@ public class C03_ExtentReports extends TestBase {
     protected static ExtentTest extentTest;
     @Test
     public void extentReportsTest() {
-
+        //Ekran goruntusu alma:
+        //getScreenShotAs metotu ile alinir. Bu metot Selenium dan gelir
+        //getScreenshotAs metotu TakesScreenshot api indan gelir
+        //Selenium 4 den itibaren 2 farklı sekilde ekran goruntusu alınabilir: Tum Sayfa, Özel element
+        //Olusturmus oldugumuz ReusableMetot yardimizya ister tum sayfanin, istersem belirli bir elementin ekran goruntusunu kolaylıkla alabilirim
+        //Extent Reports :
+        //Otomasyon raporları almak icin kullanılır.
+        //Extent report ayrı bir API dir. Extent reports seleniumin disindada kullanilabirlar.  Bu API dan gelen metotlar yardımıyla rapor sablonu oluşturabilir.
+        //3 class kullandık
+        //ExtentReports -> sablonu olusturu
+        //ExtentHTMLReporter -> sablonu projeye ekler
+        //ExtentTest -> raporlama islemini yapar. Loglari rapora yazdırir.
 
 //      REPORT  PATH
             String currentTime = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
@@ -39,6 +51,24 @@ public class C03_ExtentReports extends TestBase {
 //        ********************************RAPOR AYARLARI BITTI****************************
 //        Raporu projeme ekliyorum
             extentReports.attachReporter(extentHtmlReporter);
+            //Extent test objesini olusturduk
+            extentTest=extentReports.createTest("Extent Report login Test","Smoke Test Raporu");
+            //tum ayarlar bitti.EXTENT TEST OBJESI ILE LOGLAMA(RAPORA YAZDIRMA) ISLEMINI YAPABILIRIM
+        extentTest.pass("Kullanici anasayfaya gider");
+        driver.get("https://www.techproeducation.com");
+
+        //Lms sayfasina gidelim
+        extentTest.pass("kullanici lms  sayfasina gider");
+        driver.findElement(By.linkText("LMS LOGIN")).click();
+
+        //test bitti
+        extentTest.pass("Test basariyla gerceklesti");
+
+        //Raporu gostermek icin. Raporun olusmasi icin bu adim zorunludur.
+        extentReports.flush();
+
+
 
         }
+
 }
---
+++ src/test/java/day15/C02_WebTables.java
@@ -0,0 +1,52 @@
+package day15;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import utilities.TestBase;
+
+import java.util.List;
+
+public class C02_WebTables extends TestBase {
+    //https://the-internet.herokuapp.com/tables
+    //Create a class: WebTables
+    //Task 1 : Table1’i print edin
+    //Task 2 : 3. Row’ datalarını print edin
+    //Task 3 : Son row daki dataları print edin
+    //Task 4 : 5. Column datalarini print edin
+    //Task 5 : Iki parametreli bir Java metot oluşturalım: printData
+    //Parameter 1 = row numarasi
+    //Parameter 2 = column numarasi
+    //printData(2,3);  => 2nd row, 3rd column daki datayı print etsin
+
+    @Test
+    public void table1Print() {
+        driver.get("https://the-internet.herokuapp.com/tables");
+      String table01=  driver.findElement(By.xpath("//table[@id='table1']")).getText();
+        System.out.println("TABLE 1 VERILERI==>"+table01);
+      List<WebElement> tumVeriler=  driver.findElements(By.xpath("//table[@id='table1']//td"));
+        tumVeriler.stream().forEach(t-> System.out.println(t.getText()));
+    }
+
+    @Test
+    public void row3Print() {
+        driver.get("https://the-internet.herokuapp.com/tables");
+        List<WebElement> row3Elements = driver.findElements(By.xpath("//table[@id='table1']//tbody//tr[3]//td"));
+        row3Elements.stream().forEach(veri-> System.out.println(veri.getText()));
+
+    }
+
+    @Test
+    public void sonSutunVerileri() {
+        driver.get("https://the-internet.herokuapp.com/tables");
+        List<WebElement> sonsutunElemenalri = driver.findElements(By.xpath("//table[@id='table1']//tbody//tr[last()]//td"));
+        sonsutunElemenalri.stream().forEach(veri-> System.out.println(veri.getText()));
+    }
+
+    @Test
+    public void sutun5() {
+        driver.get("https://the-internet.herokuapp.com/tables");
+        List<WebElement> sutun5 = driver.findElements(By.xpath("//table[@id='table1']//tbody//tr//td[5]"));
+        sutun5.forEach(veri-> System.out.println(veri.getText()));
+    }
+}
